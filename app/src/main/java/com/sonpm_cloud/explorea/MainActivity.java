0d8c86930b11eb8c0d964156adf06b5e9afbb7b1
Marcin Jeznach <Scheissig@gmail.com>
2019-10-16 00:52:32 +0200

    Optimize imports in MainActivity

--- app/src/main/java/com/sonpm_cloud/explorea/MainActivity.java
+++ app/src/main/java/com/sonpm_cloud/explorea/MainActivity.java
@@ -1,30 +1,21 @@
 package com.sonpm_cloud.explorea;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
