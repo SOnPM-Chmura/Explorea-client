42d87c36f642371047897598bef451638140cc3a
Marcin Jeznach <Scheissig@gmail.com>
2019-10-15 23:30:10 +0200

    Prepare project for login functionality

--- app/src/main/java/com/sonpm_cloud/explorea/MainActivity.java
+++ app/src/main/java/com/sonpm_cloud/explorea/MainActivity.java
@@ -2,13 +2,27 @@ package com.sonpm_cloud.explorea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static GoogleSignInAccount account = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null
+        account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
     }
 }
